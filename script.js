9343ff0d54245338c40d0394d2ca1d5bc1feb82b
Davit Shavishvili <taqtaqva001@gmail.com>
2023-11-08 22:05:00 +0400

    add calc

--- script.js
+++ script.js
@@ -13,3 +13,27 @@ xMark.addEventListener("click", () => {
     burger.style.display = "block";
     xMark.style.display = "none";
 })
+
+
+const display = document.getElementById('inputBox')
+const buttons = document.querySelectorAll(".calcbutton")
+const specialChars = ["%", "*", "/", "-", "+", "="];
+let output = "";
+
+const calculate = (btnValue) => {
+    if (btnValue === "=" && output !== "") {
+        output = eval(output.replace("%", "/100"));
+    } else if (btnValue === "AC") {
+        output = "";
+    } else if (btnValue === "DEL") {
+        output = output.toString().slice(0,-1);
+    } else {
+        if (output === "" && specialChars.includes(btnValue)) return;
+        output += btnValue;
+    }
+    display.value = output;
+}
+
+buttons.forEach((button) => {
+    button.addEventListener("click", (e) => calculate(e.target.dataset.value));
+})
